19d25c9e6a79f6c3f342d53e916a94fa27c01752
Juan Alderete <juan41285@gmail.com>
2016-11-15 00:12:39 -0300

    error siempre abre el 2 y no suma

--- app/models/desafio.js
+++ app/models/desafio.js
@@ -1,6 +1,5 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   nombre: attr('string'),
@@ -10,9 +9,7 @@ export default Model.extend({
   consignaInicial: attr('string'),
   escena: attr('string'),                 // escena que se tiene que cargar, por ejemplo: 'AlienInicial'
   actividad: attr(),                       // se completa desde el hook del desafío.
-  esActivo: attr('integer'),
-
-  isComplete: Ember.computed.equal('id', null)
+  esActivo: attr('integer')
 });
 
 
--- app/routes/desafios/index.js
+++ app/routes/desafios/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('desafio');
-    console.log('lklklklklkl', this.store)
+ 
   }
 });
